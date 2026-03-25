f5a7fa65a6c3c60ec4d92211cc2cead1c9dc989f
Christi Reid <christireid@gmail.com>
2026-03-25 14:06:27 -0400

    Disable browser scroll restoration so page always loads at top

--- src/client.tsx
+++ src/client.tsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
 
+history.scrollRestoration = 'manual'
+window.scrollTo(0, 0)
+
 const root = document.getElementById('root')!
 createRoot(root).render(
   <React.StrictMode>
